3f2f4e951b013a6ac1553260e052e64cd7fe84fb
Kostja P <kpavlovsky@users.noreply.github.com>
2023-10-19 00:38:03 +0200

    reqs

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
 from fastapi import FastAPI
 import socket
+from datetime import datetime
 
 app = FastAPI()
 
 @app.get("/")
 async def get_server_hostname():
     hostname = socket.gethostname()
-    return {"hostname": hostname}
-
-
+    return {"hostname": hostname, "current_time": datetime.now().isoformat()}
